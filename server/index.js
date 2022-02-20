5c97f8dcb722983fc8080b3884a6ab30991698e1
Raja <rajavn6789@gmail.com>
2022-02-20 13:54:52 +0530

    Code formatting

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const ACCNodeWrapper = require("acc-node-wrapper");
 const wrapper = new ACCNodeWrapper();
 const utilFunctions = require("./utils/functions");
 
-
 const { getCarDetails, getTrackGripStatus } = utilFunctions;
 
 /* Web Sockets */
@@ -18,48 +17,50 @@ const startWSSServer = () => {
     });
 
     ws.on("message", (data) => {
-        console.log("UI connected and ready to receive");
-        setInterval(() => {
-          const { m_physics_result, m_graphics_result, m_static_result } =
-            wrapper.getAllSharedMemory();
-          const carModel = m_static_result.carModel.join("");
-          const result = {
-            gas: m_physics_result.gas,
-            brake: m_physics_result.brake,
-            speed: Math.round(m_physics_result.speedKmh),
-            tc: m_physics_result.tc,
-            abs: m_physics_result.abs,
-            gear: m_physics_result.gear,
-            rpm: m_physics_result.rpms,
-            isEngineRunning: m_physics_result.rpms > 1000,
-            steerAngle: Math.round(
-              getCarDetails(carModel).maxSteering * m_physics_result.steerAngle
-            ), 
-            ffb: Math.round(Math.abs(m_physics_result.finalFF * 100)),
-            carDamage: m_physics_result.carDamage.map((val) => Math.round(val)),
-            suspensionTravel: m_physics_result.suspensionTravel.map(
-              (item) => item * 1000
-            ),
-            wheelAngularSpeed: m_physics_result.wheelAngularSpeed
-              .map((item) => Math.abs(Math.round(item)))
-              .slice(2, 4),
-            accG: m_physics_result.accG.map(
-              (val) => Math.floor((val + Number.EPSILON) * 100) / 100
-            ),
-            time: m_graphics_result.iCurrentTime / 100,
-            normalizedCarPosition: m_graphics_result.normalizedCarPosition,
-            trackGripStatus: getTrackGripStatus(m_graphics_result.trackGripStatus),
-            smVersion: m_static_result.smVersion.join(""),
-            acVersion: m_static_result.acVersion.join(""),
-            carModel: getCarDetails(carModel).name,
-            track: m_static_result.track.join(""),
-            playerName: m_static_result.playerName.join(""),
-            playerSurname: m_static_result.playerSurname.join(""),
-            playerNick: m_static_result.playerNick.join(""),
-            isMultiplayer: m_static_result.isOnline,
-          };
-          ws.send(JSON.stringify(result));
-        }, 1000 / 20);
+      console.log("UI connected and ready to receive");
+      setInterval(() => {
+        const { m_physics_result, m_graphics_result, m_static_result } =
+          wrapper.getAllSharedMemory();
+        const carModel = m_static_result.carModel.join("");
+        const result = {
+          gas: m_physics_result.gas,
+          brake: m_physics_result.brake,
+          speed: Math.round(m_physics_result.speedKmh),
+          tc: m_physics_result.tc,
+          abs: m_physics_result.abs,
+          gear: m_physics_result.gear,
+          rpm: m_physics_result.rpms,
+          isEngineRunning: m_physics_result.rpms > 1000,
+          steerAngle: Math.round(
+            getCarDetails(carModel).maxSteering * m_physics_result.steerAngle
+          ),
+          ffb: Math.round(Math.abs(m_physics_result.finalFF * 100)),
+          carDamage: m_physics_result.carDamage.map((val) => Math.round(val)),
+          suspensionTravel: m_physics_result.suspensionTravel.map(
+            (item) => item * 1000
+          ),
+          wheelAngularSpeed: m_physics_result.wheelAngularSpeed
+            .map((item) => Math.abs(item))
+            .slice(2, 4),
+          accG: m_physics_result.accG.map(
+            (val) => Math.floor((val + Number.EPSILON) * 100) / 100
+          ),
+          time: m_graphics_result.iCurrentTime / 100,
+          normalizedCarPosition: m_graphics_result.normalizedCarPosition,
+          trackGripStatus: getTrackGripStatus(
+            m_graphics_result.trackGripStatus
+          ),
+          smVersion: m_static_result.smVersion.join(""),
+          acVersion: m_static_result.acVersion.join(""),
+          carModel: getCarDetails(carModel).name,
+          track: m_static_result.track.join(""),
+          playerName: m_static_result.playerName.join(""),
+          playerSurname: m_static_result.playerSurname.join(""),
+          playerNick: m_static_result.playerNick.join(""),
+          isMultiplayer: m_static_result.isOnline,
+        };
+        ws.send(JSON.stringify(result));
+      }, 1000 / 20);
     });
 
     ws.on("close", () => {
